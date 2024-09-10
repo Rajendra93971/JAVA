34f4a15a02b14bf5e76541ea0fbf8469fa6cb627
Rajendra Mewada <rajputrajendra929@gmail.com>
2024-09-11 01:56:01 +0530

    Add files via upload
    Lots of files.... something 500 in a month thanks all of you

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,28 +1,34 @@
-package gfg50_array;
+package com.kunal;
 
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] a = {2, 5, 7, 9, 12, 14, 16, 17, 19, 20, 24, 28};
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
 
-        int srch = 16;
-        int li = 0;
-        int hi = a.length - 1;
-        int mi = (li + hi) / 2;
-        while (li <= hi) {
-            if (a[mi] == srch) {
-                System.out.println("Ele at the posw " + mi + "index position");
-            } else if (a[mi] < srch) {
-                li = mi + 1;
+    // return the index
+    // return -1 if it does not exist
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
             } else {
-                hi = mi - 1;
+                // ans found
+                return mid;
             }
-            mi = (li + hi) / 2;
         }
-        if (li > hi) {
-            System.out.println("Ele not found");
-
-        }
-
+        return -1;
     }
 }
--- Swap.java
+++ Swap.java
@@ -1,21 +1,32 @@
-class Swap
-{
-    public static void main(String args[])
-    {
+package com.kunal;
 
-      int a=1;
-      int b=7;
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
 
-      System.out.println("Before swapping");
-      System.out.println(a);
-      System.out.println(b);
+        // swap numbers code
+//        int temp = a;
+//        a = b;
+//        b = temp;
 
-      a=a+b;
-      b=a-b;
-      a=a-b;
+        swap(a, b);
 
-      System.out.println("After swapping");
-      System.out.println(a);
-      System.out.println(b);
-     }
+        System.out.println(a + " " + b);
+
+        String name = "Kunal Kushwaha";
+        changeName(name);
+        System.out.println(name);
+    }
+
+    static void changeName(String naam) {
+        naam = "Rahul Rana"; // creating a new object
+    }
+
+    static void swap(int num1, int num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        // this change will only be valid in this function scope only.
+    }
 }
